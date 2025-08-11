1fa288bcc840d0e0ec46e48f7ea9f049351c1e45
quiet-cmd <max.zh.petrov@gmail.com>
2025-08-11 21:29:56 +0300

    feat: добавил ProfilePage

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { signIn } from '../services/firebase/auth';
 
-interface LoginFormValues {
+export interface LoginFormValues {
   email: string;
   password: string;
 }
